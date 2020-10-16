2769c1a6fb5c5aad04d5412552f56176a6bc6f83
Serge Zaitsev <zaitsev.serge@gmail.com>
2020-10-16 08:09:12 +0200

    fix octave

--- n.js
+++ n.js
@@ -13,7 +13,7 @@ play = (s, bpm) => {
   for (m of s.matchAll(/(\d*)?(\.?)(#?)([a-g-])(\d*)/g)) {
     k = m[4].charCodeAt(); // note ASCII [0x41..0x47] or [0x61..0x67]
     n = 0 | ((((k & 7) * 1.6 + 8) % 12) + !!m[3] + 12 * c(m[5], 1, 3)); // note index [0..35]
-    v(z.frequency, 65.4 * 2 ** (n / 12));
+    v(z.frequency, 261.63 * 2 ** (n / 12));
     v(g.gain, (~k & 8) / 8);
     t = t + (240 / bpm / c(m[1] || 4, 1, 64)) * (1 + !!m[2] / 2);
     v(g.gain, 0);
